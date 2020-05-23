0556aafadabe1e4d03e38947272ea33a88e5667d
rovm <rovm92@naver.com>
2020-05-23 22:33:07 +0900

    화면 전환
    add: 화면 전환

--- hello/app/src/main/java/com/example/hello/MainActivity.kt
+++ hello/app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener{
+            startActivity(Intent(this@MainActivity, BmiJavaActivity::class.java))
+        }
 
+        button2.setOnClickListener{
+            startActivity(Intent(this@MainActivity, BmiKotlinActivity::class.java))
         }
     }
 }
