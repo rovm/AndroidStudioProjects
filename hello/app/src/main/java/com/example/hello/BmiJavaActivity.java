73e54e97e2ecbc048027fc1c0fbdf25ca828dad2
rovm <rovm92@naver.com>
2020-05-23 23:02:30 +0900

    Update BmiJavaActivity.java

--- hello/app/src/main/java/com/example/hello/BmiJavaActivity.java
+++ hello/app/src/main/java/com/example/hello/BmiJavaActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class BmiJavaActivity extends AppCompatActivity {
+    //불필요한 findViewById 함수를 피하기 위해 UI 요소를 멤버 변수로 가지고 있는다.
+    EditText tallField;
+    EditText weightField;
+    TextView resultLabel;
+    Button bmiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,26 @@ public class BmiJavaActivity extends AppCompatActivity {
         //  UI로 사용할 레이아웃 XML 파일을 지정
         setContentView(R.layout.layout_view_binding);
 
+        tallField = findViewById(R.id.tallFileld);
+        weightField = findViewById(R.id.weightField);
+        resultLabel = findViewById(R.id.resultLabel);
+        bmiButton = findViewById(R.id.bmiButton);
+
         // bmiButton 이 클릭된 경우의 이벤트 리스너를 등록한다.
-        findViewById(R.id.bmiButton).setOnClickListener(new View.OnClickListener() {
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tall = tallField.getText().toString();
+
+                String weight = weightField.getText().toString();
+
+                double bmi = Double.parseDouble(weight) / Math.pow(Double.parseDouble(tall) / 100.0 ,2);
+
+                // 결과 bmi 를 resultLabel에 보여준다.
+                resultLabel.setText("키: " + tall + ", 체중: " + weight + ", BMI: " + bmi);
+            }
+         });
+        /*findViewById(R.id.bmiButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText tallField = findViewById(R.id.tallFileld);
@@ -31,6 +57,6 @@ public class BmiJavaActivity extends AppCompatActivity {
                 TextView resultLabel = findViewById(R.id.resultLabel);
                 resultLabel.setText("키: " + tall + ", 체중: " + weight + ", BMI: " + bmi);
             }
-        });
+        });*/
     }
 }
