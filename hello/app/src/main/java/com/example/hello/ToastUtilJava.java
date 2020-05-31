a368670945ae0f33d9f7680383f55dbdf479e261
rovm <rovm92@naver.com>
2020-05-31 22:56:31 +0900

    4.6.05 Java 함수의 디폴트 파라미터
    메소드 오버라이딩 toast 함수

--- hello/app/src/main/java/com/example/hello/ToastUtilJava.java
+++ hello/app/src/main/java/com/example/hello/ToastUtilJava.java
@@ -3,11 +3,22 @@ package com.example.hello;
 import android.widget.Toast;
 
 public class ToastUtilJava {
+    //짧은 토스트 메세지를 보여주는 함수
     public static void toastShort(String message){
         Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    //긴 토스트 메세지를 보여주는 함수
     public static void toastLong(String message){
         Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    //파라미터에 따라 긴 토스트 메세지, 또는 짧은 토스트 메세지를 보여준다.
+    public static void toast(String message, int length){
+        Toast.makeText(MainApplication.getAppContext(), message, length).show();
+    }
+
+    public static void toast(String message){
+        toast(message, Toast.LENGTH_SHORT);
     }
 }
