17ee403a2641273252ec4a3ed91b421fc92cb89e
rovm <rovm92@naver.com>
2020-05-31 22:40:16 +0900

    4.6.03 Kotlin의 최상위 함수
    add 코틀린 util함수 만들기

---
+++ hello/app/src/main/java/com/example/hello/ToastutilKotiln.kt
@@ -0,0 +1,11 @@
+package com.example.hello
+
+import android.widget.Toast
+
+fun toastShort(message:String){
+    Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+fun toastLong(message:String){
+    Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_LONG).show()
+}
