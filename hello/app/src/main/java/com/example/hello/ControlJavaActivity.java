d49fe987b9209148a95913819de8d79954986107
rovm <rovm92@naver.com>
2020-05-31 20:29:04 +0900

    4.5.03 Java의 if, else, switch
    add 4.5.03 Java의 if, else, switch

--- hello/app/src/main/java/com/example/hello/ControlJavaActivity.java
+++ hello/app/src/main/java/com/example/hello/ControlJavaActivity.java
@@ -3,12 +3,53 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ControlJavaActivity extends AppCompatActivity {
+
+    //불필요한 findViewById 함수를 피하기 위해 UI 요소를 멤버 변수로 가지고 있는다.
+    EditText numberField;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_control);
+
+        //UI 요소 멤버변수를 Xml 레이아웃에서 findViewById 함수를 이용해 바인딩한다.
+        numberField = findViewById(R.id.numberField);
+        button = findViewById(R.id.button);
+
+        //버튼이 클릭되었을때의 이벤트리스너를 설정한다.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(numberField.getText().toString());
+
+                //if, else if, else 문으로 2의 배수, 3의 배수를 체크해 서로 다른 토스트 메세지를 보여준다.
+                if(number % 2 == 0){
+                    Toast.makeText(getApplicationContext(), "" + number + "는 2의 배수입니다.", Toast.LENGTH_SHORT).show();
+                } else if(number % 3 == 0){
+                    Toast.makeText(getApplicationContext(), "" + number + "는 3의 배수입니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "" + number, Toast.LENGTH_SHORT).show();
+                }
+
+                switch (number){
+                    case 4:
+                        button.setText("실행 - 4");
+                        break;
+                    case 9:
+                        button.setText("실행 - 9");
+                        break;
+                    default:
+                        button.setText("실행");
+                        break;
+                }
+            }
+        });
     }
 }
--- hello/app/src/main/java/com/example/hello/ControlKotlinActivity.kt
+++ hello/app/src/main/java/com/example/hello/ControlKotlinActivity.kt
@@ -2,11 +2,44 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.layout_control.*
 
 class ControlKotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_control)
+
+        // 버튼이 클릭되었을때의 이벤트리스너를 설정한다.
+        button.setOnClickListener {
+            //numberField의 값을 읽어 int 형으로 반환한다.
+            val number = numberField.text.toString().toInt()
+
+            //if, else if, else 문으로 2의 배수, 3의 배수를 체크해 서로 다른 토스트 메세지를 보여준다.
+//            if(number % 2 == 0){
+//                Toast.makeText(applicationContext, "${number} 는 2의 배수입니다.", Toast.LENGTH_SHORT).show()
+//            } else if(number % 3 == 0){
+//                Toast.makeText(applicationContext, "${number} 는 3의 배수입니다.", Toast.LENGTH_LONG).show()
+//            } else {
+//                Toast.makeText(applicationContext, "${number}", Toast.LENGTH_LONG).show()
+//            }
+
+            //when 문은 케이스로 조건식도 사용 가능
+            when {
+                number % 2 == 0 -> Toast.makeText(applicationContext, "${number} 는 2의 배수입니다.", Toast.LENGTH_SHORT).show()
+                number % 3 == 0 ->  Toast.makeText(applicationContext, "${number} 는 3의 배수입니다.", Toast.LENGTH_LONG).show()
+                else -> Toast.makeText(applicationContext, "${number}", Toast.LENGTH_LONG).show()
+            }
+
+            //코틀린에서는 switch 문을 대체해 when을 사용할 수 있다.
+            when (number){
+                in 1..4 -> button.text = "실행 - 4"
+                9 , 18-> {
+                    button.text = "실행 - 9"
+                }
+                else -> button.text = "실행"
+            }
+        }
     }
 }
