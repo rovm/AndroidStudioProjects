bc7562f18de4d6f5d3f16fc46d3a4cd7e718b9ec
rovm <rovm92@naver.com>
2020-05-31 19:59:12 +0900

    4.5.02 개별 액티비티
    add 4.5.02 개별 액티비티

---
+++ hello/app/src/main/java/com/example/hello/ControlKotlinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ControlKotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_control)
+    }
+}
