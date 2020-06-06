4f23c9c252caaa3a80bbed6831d0dc79f7d037dd
rovm <rovm92@naver.com>
2020-06-06 21:41:39 +0900

    update intent
    updatw intent

--- createLottoNumber/app/src/main/java/com/example/createlottonumber/SettingScreen.kt
+++ createLottoNumber/app/src/main/java/com/example/createlottonumber/SettingScreen.kt
@@ -2,11 +2,15 @@ package com.example.createlottonumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SettingScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_screen)
+
+        Toast.makeText(applicationContext, "SettingScreen 입니다.", Toast.LENGTH_LONG).show()
+
     }
 }
