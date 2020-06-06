e684075ec37861d17e24748954fd6853c9c2c511
rovm <rovm92@naver.com>
2020-06-06 20:01:51 +0900

    Create Project Lotto Number
    create lotto project

---
+++ createLottoNumber/app/src/main/java/com/example/createlottonumber/ResultScreen.kt
@@ -0,0 +1,12 @@
+package com.example.createlottonumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_screen)
+    }
+}
---
+++ createLottoNumber/app/src/main/java/com/example/createlottonumber/SettingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.createlottonumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_screen)
+    }
+}
