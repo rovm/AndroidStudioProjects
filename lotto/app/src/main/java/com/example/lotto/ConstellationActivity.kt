2557903b46232f0089f3c5321aaeefdd60150015
rovm <rovm92@naver.com>
2020-06-18 20:34:05 +0900

    add 화면 구성
    화면 추가(main, name, result, constellation)
    
    2020.06.18

--- lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class ConstellationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(applicationContext, "mainActivity 입니다.", Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_constellation)
+        Toast.makeText(applicationContext, "ConstellationActivity 입니다.", Toast.LENGTH_LONG).show()
     }
 }
